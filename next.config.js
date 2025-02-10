f087c20a89e4580b882f118fde0dfb71a45a36b7
brokenankle94 <chrislee9407@gmail.com>
2025-02-10 15:30:20 +0900

    Setup GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/K_Actuary_AI_Agent',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
