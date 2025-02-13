45b4c84f2a667b27009307a5c9ad61f79e055755
brokenankle94 <chrislee9407@gmail.com>
2025-02-13 12:12:05 +0900

    소개글 업데이트

--- src/app.py
+++ src/app.py
@@ -187,8 +187,8 @@ def main():
     with st.sidebar:
         st.markdown("### 소개")
         st.markdown("""
-        - 본 AI 챗 서비스는 한국 계리업무를 수행하는 계리사를 위해 개발된 개인 프로젝트 기반 AI Chat / Agent입니다.
-        - 현재 다양한 유용한 기능들이 지속적으로 개발 중이며, 보다 향상된 서비스를 제공하기 위해 개선을 이어가고 있습니다.
+        - 본 AI 챗 서비스는 한국 계리업무를 수행하는 계리사를 위해 개발된 개인 프로젝트로 만들어진 AI Chatbot / Agent입니다.
+        - RAG(검색증강생성)에 더불어 현재 다양한 유용한 기능들이 지속적으로 개발 중이며, 보다 향상된 서비스를 제공하기 위해 개선을 이어가고 있습니다.
         """)
         st.markdown('<p style="color: black; font-size: 12px;">- 현재 API 비용이 가장 저렴한 LLM인 gpt3.5-turbo를 사용하고 있어 기대보다 성능이 떨어질 수 있습니다.</p>', unsafe_allow_html=True)
         st.markdown('<p style="color: red; font-size: 12px;">* (주의) 본 AI가 제공하는 답변은 참고용이며, 정확성을 보장할 수 없습니다. 보안을 위해 회사 기밀, 개인정보등은 제공하지 않기를 권장드리며, 반드시 실제 업무에 적용하기 전에 검토하시길 바랍니다.</p>', unsafe_allow_html=True)
