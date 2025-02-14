207208cc49ef8eb54db91ff6bd36f2ce797c3ded
brokenankle94 <chrislee9407@gmail.com>
2025-02-14 09:34:39 +0900

    구조도 보여지는방법 수정

--- src/app.py
+++ src/app.py
@@ -201,9 +201,15 @@ def main():
         st.markdown('[개발자 Linkedin](https://www.linkedin.com/in/chrislee9407/)')
         st.markdown('[K-계리 AI 플랫폼](https://chrischangminlee.github.io/K_Actuary_AI_Agent_Platform/)')
         
-        # 이미지 링크
+        # 이미지 표시
         if os.path.exists("KActuaryAgentStructure_021425.png"):
-            st.markdown('[챗봇 개발 구조도](KActuaryAgentStructure_021425.png)')
+            st.markdown("챗봇 개발 구조도")
+            st.markdown(
+                f'<a href="KActuaryAgentStructure_021425.png" target="_blank">'
+                f'<img src="KActuaryAgentStructure_021425.png" width="100%">'
+                f'</a>',
+                unsafe_allow_html=True
+            )
         
         st.markdown("---")
         st.markdown("### 참고된 pdf")
